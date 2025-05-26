a579903066fdae75bf6bf7125c77922bf365a0b7
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-26 10:25:44 -0300

    Devolvendo series no navegador

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,20 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repository;
     @GetMapping("/series")
-    public String getSeries() {
-        return "Aqui serao listadas as series";
+    public List<Serie> getSeries() {
+        return repository.findAll();
     }
 
 }
