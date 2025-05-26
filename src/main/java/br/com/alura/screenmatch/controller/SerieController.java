43431e16baba28a61660041d28834815c027f762
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-26 11:24:10 -0300

    Exibindo as top series do banco

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,17 +4,24 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
     @Autowired
     private SeriesService seriesService;
-    @GetMapping("/series")
+
+    @GetMapping("")
     public List<SerieDTO> getSeries() {
         return seriesService.getAllSeries();
     }
 
+    @GetMapping("/top5")
+    public List<SerieDTO> getTop5Series() {
+        return seriesService.getTop5Series();
+    }
 }
--- src/main/java/br/com/alura/screenmatch/service/SeriesService.java
+++ src/main/java/br/com/alura/screenmatch/service/SeriesService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,15 @@ public class SeriesService {
     private SerieRepository serieRepository;
 
     public List<SerieDTO> getAllSeries() {
-        return serieRepository.findAll()
+        return convertData(serieRepository.findAll());
+    }
+
+    public List<SerieDTO> getTop5Series() {
+        return  convertData(serieRepository.findTop5ByOrderByAvaliationDesc());
+    }
+
+    private List<SerieDTO> convertData(List<Serie> series) {
+        return series
                 .stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitle(), s.getTotalSeasons(), s.getAvaliation(),
                         s.getGenre(), s.getDirector(), s.getActors(), s.getPlot(), s.getLanguage(), s.getAwards(),
